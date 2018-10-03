ba4c9bb92aead1471aa53f12f27628805fe7253c
ivan4000 <sxln1@mail.ru>
2018-10-03 19:08:49 +0300

    JL20arrays first commit

---
+++ src/ArrayExample.java
@@ -0,0 +1,9 @@
+public class ArrayExample {
+    int[] arr={3,4,5,6,};
+    boolean[] booleanArray={true,true,false};
+    double[]doubleArray={234.234,15,123,6534.34};
+    int array[]={3,54,65,3,32,23};
+    int [][]arrayTwo={{123,23,245},{123,4525,23}};
+    int []arrayThgree[]={{123,23,245},{123,4525,23}};
+    int arrayFour[][]={{123,23,245},{123,4525,23}};
+}
